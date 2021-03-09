f26390229eab50fd6971f037004e9d3c8197e0fe
Motisma479 <74106176+Motisma479@users.noreply.github.com>
2021-03-09 19:53:34 +0100

    Add files via upload

---
+++ sketch_jan20a.ino
@@ -0,0 +1,54 @@
+int Sortie_sevo = 13;
+int potar = A0;
+
+float sensorValue=0;
+float voltage =0;
+
+float Temperature = 0;
+float Temp_Anc = 0;
+
+long Timer = 0;
+long AncTimer_1 = 0;
+
+long Timer_2 = 0;
+long AncTimer_2 = 0;
+
+
+void setup() {
+  pinMode(Sortie_sevo,OUTPUT);
+  pinMode(potar,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  sensorValue = analogRead(A0);
+  voltage = sensorValue * (5.0 / 1023.0);
+  Temperature = voltage*7;
+   
+  servo(Temperature);
+  Timer_2=micros();
+  
+  if(Timer>AncTimer_1+500)
+  {
+     Serial.println(Temperature);
+  }
+}
+
+/***Fonction*du*servo***/
+void servo(float temperature)
+{
+  float tp = (200/7)*temperature+1000;
+  
+  Timer=micros();
+  if (Timer>AncTimer_1+tp)
+  {
+     digitalWrite(Sortie_sevo, LOW);
+  }
+  if (Timer>AncTimer_1+20000)
+  {
+     digitalWrite(Sortie_sevo, HIGH);
+     AncTimer_1=Timer;
+  }
+}
+
